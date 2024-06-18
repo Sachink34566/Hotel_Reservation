c45f4231d142b11186ca3673622cd277f2ac1381
Sachink34566 <sachinkumar98504@gmail.com>
2024-06-18 14:49:40 +0530

    Ability to find the Best Rated Hotel for a given Date Range

--- Hotel_reservation/Hotel.cs
+++ Hotel_reservation/Hotel.cs
@@ -13,16 +13,13 @@ namespace Hotel_reservation
         public int WeekEndRegular { get; set; }
         public int TotalPrice { get; set; }
         public int Rating { get; set; }
-        public int WeekDayReward { get; set; }
-        public int WeekEndReward { get; set; }
 
-        public Hotel(string name, int weekdayRegular, int weekendRegular, int weekdayReward, int weekendReward, int rating)
+
+        public Hotel(string name, int weekdayRegular, int weekendRegular,  int rating)
         {
             HotelName = name;
             WeekDayRegular = weekdayRegular;
             WeekEndRegular = weekendRegular;
-            WeekDayReward = weekdayReward;
-            WeekEndReward = weekendReward;
             Rating = rating;
             Display();
         }
@@ -31,13 +28,19 @@ namespace Hotel_reservation
         {
             Console.WriteLine($"Hotel {HotelName} with regular customer rate for Week Day is $ {WeekDayRegular} and Week End is $ {WeekEndRegular} and Rating {Rating}.");
         }
-
-        public static void CheapestRate(List<Hotel> hotels, int weekdays, int weekends, bool isRewardCustomer)
+        public static void BestRegular(List<Hotel>  hotels, int weekDays, int weekEnds)
+        {
+            int BestRating = hotels.Max((hotel) => hotel.Rating);
+            Hotel BestHotel = hotels.Single((hotel) => hotel.Rating == BestRating);
+            BestHotel.TotalPrice = (weekDays * BestHotel.WeekDayRegular) + (weekEnds * BestHotel.WeekEndRegular);
+            Console.WriteLine($"{BestHotel.HotelName}, Total Price = ${BestHotel.TotalPrice}");
+        }
+        public static void ChepeastRate(List<Hotel> hotels, int weekday, int weekend)
         {
             foreach (Hotel hotel in hotels)
             {
-                int weekdayPrice = isRewardCustomer ? weekdays * hotel.WeekDayReward : weekdays * hotel.WeekDayRegular;
-                int weekendPrice = isRewardCustomer ? weekends * hotel.WeekEndReward : weekends * hotel.WeekEndRegular;
+                int weekdayPrice = weekday * hotel.WeekDayRegular;
+                int weekendPrice = weekend * hotel.WeekEndRegular;
                 hotel.TotalPrice = weekdayPrice + weekendPrice;
             }
 
@@ -45,7 +48,7 @@ namespace Hotel_reservation
 
 
             var cheapestHotels = hotels.Where(h => h.TotalPrice == minPrice);
-            int bestRating=cheapestHotels.Max(h=>h.Rating);
+            int bestRating=cheapestHotels.Max((h=>h.Rating));
 
             cheapestHotels=from hotel in hotels
                            where hotel.Rating == bestRating
--- Hotel_reservation/Program.cs
+++ Hotel_reservation/Program.cs
@@ -11,25 +11,55 @@ namespace Hotel_reservation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation");
+            
 
             List<Hotel> hotels = new List<Hotel>()
             {
-                new Hotel("The Oberoi Udaivilas", 600,1000,80,90,3),
-                new Hotel("The Taj Mahal Palace Mumbai", 700, 1000,100,120,4),
-                new Hotel("The Leela Palace New Delhi", 800, 1100, 90, 120, 4),
-                new Hotel("The Lodhi New Delhi", 1000, 1200, 200, 180, 3),
+                new Hotel("The Oberoi Udaivilas", 600,1000,4),
+                new Hotel("The Taj Mahal Palace Mumbai", 700, 1000,5 ),
+                new Hotel("The Leela Palace New Delhi", 800, 1100,4),
+                new Hotel("The Lodhi New Delhi",1000,1200,5)
             };
-
             Calculatedays calculatedays = new Calculatedays();
-            var (weekDays, WeekEnds) = calculatedays.Count();
-            if (weekDays != -1)
+
+            Console.WriteLine("\n1.Cheapest Hotels 2.BestRated Hotels 3.Exit");
+            Console.Write("Enter the choice = ");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
             {
-                Hotel.CheapestRate(hotels, weekDays, WeekEnds,true);
-            }
-            else
-            {
-                Console.WriteLine("Invalid Dates");
+                case 1:
+                    Console.WriteLine("Finding the Cheapest hotel !");
+
+                    var (weekDays, WeekEnds) = calculatedays.Count();
+                    if (weekDays != -1)
+                    {
+                        Hotel.ChepeastRate(hotels, weekDays, WeekEnds);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Dates");
+                    }
+                    break;
+                case 2:
+                    Console.WriteLine("Finding the Best Hotels !");
+                    var (BestweekDays, BestWeekEnds) = calculatedays.Count();
+                    if (BestweekDays != -1 && BestWeekEnds != -1)
+                    {
+                        Hotel.BestRegular(hotels, BestweekDays, BestWeekEnds);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Dates");
+                    }
+                    break;
+                case 3:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
             }
         }
     }
+    
 }
