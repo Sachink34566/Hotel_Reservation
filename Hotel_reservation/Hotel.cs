47e748b1ea39ebd9ca81b90cc685c46d722c9bab
Sachink34566 <sachinkumar98504@gmail.com>
2024-06-18 12:12:34 +0530

    Add hotel to reservation system

---
+++ Hotel_reservation/Hotel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_reservation
+{
+    public class Hotel
+    {
+        public string HotelName {  get; set; }
+        public int RegularCustomerRate { get; set; }
+
+        public Hotel(string name,int rate)
+        {
+            HotelName = name;
+            RegularCustomerRate = rate;
+
+        }
+        public void Display()
+        {
+            Console.WriteLine($"Hotel '{HotelName}' with regular customer rate $ {RegularCustomerRate}.");
+        }
+    }
+}
--- Hotel_reservation/Program.cs
+++ Hotel_reservation/Program.cs
@@ -11,6 +11,19 @@ namespace Hotel_reservation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Hotel Reservation");
+
+            List<Hotel> hotels = new List<Hotel>()
+            {
+                new Hotel("The Oberoi Udaivilas", 6000),
+                new Hotel("The Taj Mahal Palace Mumbai", 700),
+                new Hotel("The Leela Palace New Delhi", 800),
+                new Hotel("The Lodhi New Delhi",1000)
+            };
+
+            foreach(Hotel hotel in hotels)
+            {
+                hotel.Display();
+            }
         }
     }
 }
